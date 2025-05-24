c0f66ec1c483befac1b4e3b75621bf5b2d8fe07f
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-05-24 16:26:19 +0300

    Create the DbSet Entities and configure composite key for OrderItems

--- RestaurantReservation.Db/Data/RestaurantReservationDbContext.cs
+++ RestaurantReservation.Db/Data/RestaurantReservationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Models;
 
 namespace RestaurantReservation.Db.Data;
 
@@ -7,5 +8,25 @@ public class RestaurantReservationDbContext : DbContext
     public RestaurantReservationDbContext(DbContextOptions<RestaurantReservationDbContext> options)
         : base(options)
     {
+    }
+    
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Reservation> Reservations { get; set; }
+    public DbSet<Restaurant> Restaurants { get; set; }
+    public DbSet<Table> Tables { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<MenuItem> MenuItems { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        // OrderItem: Composite Key
+        modelBuilder.Entity<OrderItem>()
+            .HasKey(oi => new { oi.OrderId, oi.MenuItemId });
+        
+        // I followed EF Core conventions for now so not many configurations are required
     }
 }
