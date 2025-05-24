0428b5a00d0b48bf18b96448039669f7999cb2a7
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-05-24 19:02:45 +0300

    Rename OrderPrice to TotalAmount

--- RestaurantReservation.Db/Models/Order.cs
+++ RestaurantReservation.Db/Models/Order.cs
@@ -8,7 +8,7 @@ public class Order
     }
     public int OrderId { get; set; }    
     public DateTime OrderDate { get; set; }
-    public required decimal OrderPrice { get; set; }
+    public required decimal TotalAmount { get; set; }
     public int ReservationId { get; set; }  
     public Reservation Reservation { get; set; }
     public int EmployeeId { get; set; } 
