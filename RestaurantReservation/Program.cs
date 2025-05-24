cb27251ccb4317a04cca91f5441f7e90cfdeaf34
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-05-24 16:41:40 +0300

    Add connection string

--- RestaurantReservation/Program.cs
+++ RestaurantReservation/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace RestaurantReservation;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Data;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var connectionString = config.GetConnectionString("DefaultConnection");
+
+var options = new DbContextOptionsBuilder<RestaurantReservationDbContext>()
+    .UseSqlServer(connectionString)
+    .Options;
+
+using var context = new RestaurantReservationDbContext(options);
