f4a5577c6dda7bab5e87977efba59566778e0059
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-05-24 22:03:50 +0300

    Implement the ListManagers method

---
+++ RestaurantReservation.Db/Services/Methods.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Data;
+using RestaurantReservation.Db.Models;
+
+namespace RestaurantReservation.Db.Services;
+
+public class Methods(RestaurantReservationDbContext context)
+{
+    public async Task<List<Employee>> ListManagers()
+    {
+        var managers = await context.Employees
+            .Where(e => e.Position == "Manager")
+            .ToListAsync();
+        return managers;
+    }
+    
+}
--- RestaurantReservation/Program.cs
+++ RestaurantReservation/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using RestaurantReservation.Db.Data;
+using RestaurantReservation.Db.Models;
+using RestaurantReservation.Db.Services;
+
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+var connectionString = config.GetConnectionString("DefaultConnection");
+var options = new DbContextOptionsBuilder<RestaurantReservationDbContext>()
+    .UseSqlServer(connectionString)
+    .Options;
+    
+using var context = new RestaurantReservationDbContext(options);
+
+var methods = new Methods(context);
+var result = await methods.ListManagers();
+foreach (var manager in result)
+{
+    Console.WriteLine($"Manager: {manager.FirstName} {manager.LastName}");
+}
+
+
+
+
