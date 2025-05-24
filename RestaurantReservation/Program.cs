d38da4e07c149822c2eeb6f5807952518c3d48d8
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-05-24 16:53:20 +0300

    Remove runtime code for now

--- RestaurantReservation/Program.cs
+++ RestaurantReservation/Program.cs
@@ -1,15 +1,1 @@
-﻿using Microsoft.Extensions.Configuration;
-using Microsoft.EntityFrameworkCore;
-using RestaurantReservation.Db.Data;
-
-var config = new ConfigurationBuilder()
-    .AddJsonFile("appsettings.json")
-    .Build();
-
-var connectionString = config.GetConnectionString("DefaultConnection");
-
-var options = new DbContextOptionsBuilder<RestaurantReservationDbContext>()
-    .UseSqlServer(connectionString)
-    .Options;
-
-using var context = new RestaurantReservationDbContext(options);
+﻿Console.WriteLine("Hello, World!");
