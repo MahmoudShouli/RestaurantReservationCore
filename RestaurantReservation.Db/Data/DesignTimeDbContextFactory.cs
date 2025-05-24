530a81ff778f191bb6e002f82b5a47781ff8ad49
MahmoudShouli <mahmoud.shouli.yes@gmail.com>
2025-05-24 18:24:39 +0300

    This is a factory for EF Core to create an instance of DbContext

---
+++ RestaurantReservation.Db/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using RestaurantReservation.Db.Data;
+
+namespace RestaurantReservation.Db.Data;
+
+public class DesignTimeDbContextFactory: IDesignTimeDbContextFactory<RestaurantReservationDbContext>
+{
+    public RestaurantReservationDbContext CreateDbContext(string[] args)
+    {
+        var optionsBuilder = new DbContextOptionsBuilder<RestaurantReservationDbContext>();
+        optionsBuilder.UseSqlServer("Server=localhost;Database=RestaurantReservationCore;Trusted_Connection=True;Encrypt=False;");
+
+        return new RestaurantReservationDbContext(optionsBuilder.Options);
+    }
+}
